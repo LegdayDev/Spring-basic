4df5ba3c4b3f858e98ce4cd5a78f5f3fb9033ed8
JaeYoung <chlwodud0327@naver.com>
2023-04-24 19:41:58 +0900

    인터페이스(InitializingBean, DisposableBean) 초기화,소멸

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url; //접속해야 할 url
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -19,11 +20,26 @@ public class NetworkClient {
         System.out.println("connect: " + url);
     }
     public void call(String message){
-        System.out.println("call: " + url + " message = "+message);
+        System.out.println("call: " + url + ", message = "+message);
     }
 
     //서비스 종료시 호출
     public void disconnect(){
         System.out.println("close: "+url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        //의존관계 주입이 끝나면 호출해주겠다.
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        //빈이 종료될 때 호출
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
