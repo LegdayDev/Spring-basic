5511ae3ba5db2c13e0fc2a2399fc47176e50d6a7
JaeYoung <chlwodud0327@naver.com>
2023-04-24 19:52:03 +0900

    Bean 등록시 옵션으로 초기화,소멸 메서드

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,7 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient{
 
     private String url; //접속해야 할 url
 
@@ -28,18 +26,15 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: "+url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        //의존관계 주입이 끝나면 호출해주겠다.
-        System.out.println("NetworkClient.afterPropertiesSet");
+    public void init(){
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+    public void close(){
         //빈이 종료될 때 호출
-        System.out.println("NetworkClient.destroy");
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
