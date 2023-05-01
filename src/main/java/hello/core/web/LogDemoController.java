77e14500638b3f7c62079217181428df450761e9
JaeYoung <chlwodud0327@naver.com>
2023-05-01 16:07:25 +0900

    Log 를 찍기위한 컨트롤러, 서비스 구현(request요청이 들어오지 않기 때문에 오류)

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,31 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("log-demo")
+    @ResponseBody //view가 없을때
+    public String logDemo(HttpServletRequest request){
+        String requestURL = request.getRequestURI().toString();
+        myLogger.setRequestURL(requestURL);
+
+        myLogger.log("controller test");
+        logDemoService.logic("testID");
+
+        return "ok";
+    }
+
+}
